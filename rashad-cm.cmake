7ebe9fe2ca103effd8b5a3eafeb277334a3bfe01
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-11-12 17:11:27 +0100

    trying unix makefiles

--- rashad-cm.cmake
+++ rashad-cm.cmake
@@ -14,9 +14,10 @@ set(CTEST_DASHBOARD_ROOT "/home/travis/build")
 #set(CTEST_DASHBOARD_ROOT "/tmp/temp")
 set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/orfeotoolbox/OTB")
 set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/orfeotoolbox/build")
-set(CMAKE_MAKE_PROGRAM /tmp/ninja)
+#set(CMAKE_MAKE_PROGRAM /usr/bin/make)
 set(CMAKE_COMMAND "$ENV{CMAKE_CMD}")
-set(CTEST_CMAKE_GENERATOR "Ninja")
+#set(CTEST_CMAKE_GENERATOR "Ninja")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 set(CTEST_NIGHTLY_START_TIME "20:00:00 CEST")
 set(CTEST_DROP_METHOD "http")
@@ -26,13 +27,17 @@ set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_USE_LAUNCHERS TRUE)
 
-execute_process(COMMAND "${CMAKE_COMMAND}" -E chdir ${CTEST_SOURCE_DIRECTORY} git rev-parse --abbrev-ref HEAD OUTPUT_VARIABLE GIT_BRANCH RESULT_VARIABLE rv)
+execute_process(COMMAND "${CMAKE_COMMAND}" -E chdir ${CTEST_SOURCE_DIRECTORY} "git" "branch" OUTPUT_VARIABLE GIT_BRANCH RESULT_VARIABLE rv)
 if(NOT rv EQUAL 0)
-  message(FATAL_ERROR "cannot find git branch")
-  set(GIT_BRANCH "develop")
+message(WARNING "cannot find git branch")
+set(GIT_BRANCH "develop")
+else()
+string(REPLACE "\n" " " GIT_BRANCH ${GIT_BRANCH})
+string(REGEX REPLACE "^\\*" "" GIT_BRANCH ${GIT_BRANCH})
+string(REGEX REPLACE ".detached.from.*\\)" "" GIT_BRANCH ${GIT_BRANCH})
 endif()
-
 set(CTEST_BUILD_NAME "travis-${GIT_BRANCH}")
+
 
 #set(CTEST_TEST_ARGS INCLUDE_LABEL "")
 
@@ -43,10 +48,10 @@ BUILDNAME:STRING=${CTEST_BUILD_NAME}
 CTEST_USE_LAUNCHERS:BOOL=${CTEST_USE_LAUNCHERS}
 DART_TESTING_TIMEOUT:STRING=${CTEST_TEST_TIMEOUT}
 CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
-CMAKE_C_FLAGS:STRING=-Wall -Wno-uninitialized  -Wno-unused-variable -Wno-gnu
-CMAKE_CXX_FLAGS:STRING=-Wall -Wno-deprecated -Wno-uninitialized -Wno-gnu -Wno-overloaded-virtual -Wno-cpp -Wno-unused-parameter
+CMAKE_C_FLAGS:STRING=-Wall -Wno-uninitialized  -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING=-Wall -Wno-deprecated -Wno-uninitialized -Wno-overloaded-virtual -Wno-cpp -Wno-unused-parameter
 CMAKE_PREFIX_PATH:PATH=$ENV{XDK_DIR}
-CMAKE_MAKE_PROGRAM:FILEPATH=/tmp/ninja
+##CMAKE_MAKE_PROGRAM:FILEPATH=/tmp/ninja
 CMAKE_INSTALL_PREFIX=${CTEST_DASHBOARD_ROOT}/orfeotoolbox/install
 BUILD_TESTING:BOOL=ON
 DOTB_BUILD_DEFAULT_MODULES:BOOL=OFF
